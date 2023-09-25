3b46fcfa5df6aeb4fea42c57ee341b244bc3af7e
Victor Galindo <victor.galindo@abzu.ai>
2023-09-25 20:22:38 +0000

    update inference script

--- examples/inference.py
+++ examples/inference.py
@@ -1,24 +1,21 @@
-
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from exllamav2 import(
+from exllamav2 import (
     ExLlamaV2,
     ExLlamaV2Config,
     ExLlamaV2Cache,
     ExLlamaV2Tokenizer,
 )
 
-from exllamav2.generator import (
-    ExLlamaV2BaseGenerator,
-    ExLlamaV2Sampler
-)
+from exllamav2.generator import ExLlamaV2BaseGenerator, ExLlamaV2Sampler
 
 import time
 
 # Initialize model and cache
 
-model_directory =  "/mnt/str/models/_exl2/openllama-3b-3.0bpw-h6-exl2/"
+model_directory = "/workspace/.cache/huggingface/hub/models--TheBloke--orca_mini_v3_7B-GPTQ/snapshots/dffc6e1aa4fb048bed3f39f354edbc1606cb9bb0/"
 
 config = ExLlamaV2Config()
 config.model_dir = model_directory
@@ -26,10 +23,11 @@ config.prepare()
 
 model = ExLlamaV2(config)
 print("Loading model: " + model_directory)
+model.embed_cpu = False
 
 # allocate 18 GB to CUDA:0 and 24 GB to CUDA:1.
 # (Call `model.load()` if using a single GPU.)
-model.load([18, 24])
+model.load()
 
 tokenizer = ExLlamaV2Tokenizer(config)
 
@@ -42,7 +40,7 @@ generator = ExLlamaV2BaseGenerator(model, cache, tokenizer)
 # Generate some text
 
 settings = ExLlamaV2Sampler.Settings()
-settings.temperature = 0.85
+settings.temperature = 0.8
 settings.top_k = 50
 settings.top_p = 0.8
 settings.token_repetition_penalty = 1.15
@@ -50,16 +48,18 @@ settings.disallow_tokens(tokenizer, [tokenizer.eos_token_id])
 
 prompt = "Our story begins in the Scottish town of Auchtermuchty, where once"
 
-max_new_tokens = 150
+max_new_tokens = 300
 
 generator.warmup()
 time_begin = time.time()
 
-output = generator.generate_simple(prompt, settings, max_new_tokens, seed = 1234)
+output = generator.generate_simple(prompt, settings, max_new_tokens, seed=1234)
 
 time_end = time.time()
 time_total = time_end - time_begin
 
 print(output)
 print()
-print(f"Response generated in {time_total:.2f} seconds, {max_new_tokens} tokens, {max_new_tokens / time_total:.2f} tokens/second")
+print(
+    f"Response generated in {time_total:.2f} seconds, {max_new_tokens} tokens, {max_new_tokens / time_total:.2f} tokens/second"
+)
